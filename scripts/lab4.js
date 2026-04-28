9c7e92fff2a4b7d12994488a220fd724761fea80
ATwistedJoke <oatis@ucsd.edu>
2026-04-28 10:33:38 -0700

    Change #3

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,6 +6,9 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+    if(add !== true){
+        return false; 
+    }
     if (add) {
         let result = 0;
 
